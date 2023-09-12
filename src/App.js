4c0903cc281a028b93e161a6754fb7116e4f60ca
Alex Tsi <alex.tsi.dev@gmail.com>
2023-09-12 20:07:52 +0200

    optimize components dependencies

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import useHttpCaller from "./hooks/use-http-caller";
 function App() {
     const [tasks, setTasks] = useState([]);
 
-    const transformTasks = taskObject => {
-        const loadedTasks = [];
-
-        for (const taskKey in taskObject) {
-            loadedTasks.push({id: taskKey, text: taskObject[taskKey].text});
-        }
-
-        setTasks(loadedTasks);
-    }
-
-    const {isLoading, error, sendRequest: fetchTasks} = useHttpCaller(
-        {url: 'https://task-list-beb50-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'},
-        transformTasks
-    )
+    const {isLoading, error, sendRequest: fetchTasks} = useHttpCaller()
 
     useEffect(() => {
-        fetchTasks();
-    }, []);
+        const transformTasks = taskObject => {
+            const loadedTasks = [];
+
+            for (const taskKey in taskObject) {
+                loadedTasks.push({id: taskKey, text: taskObject[taskKey].text});
+            }
+
+            setTasks(loadedTasks);
+        }
+
+        fetchTasks(
+            {url: 'https://task-list-beb50-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'},
+            transformTasks
+        );
+    }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
         setTasks((prevTasks) => prevTasks.concat(task));
--- src/hooks/use-http-caller.js
+++ src/hooks/use-http-caller.js
@@ -1,10 +1,10 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
-const useHttpCaller = (requestConfig, dataAction) => {
+const useHttpCaller = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const sendRequest = async (taskText) => {
+    const sendRequest = useCallback(async (requestConfig, dataAction) => {
         setIsLoading(true);
         setError(null);
         try {
@@ -27,7 +27,7 @@ const useHttpCaller = (requestConfig, dataAction) => {
             setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
-    };
+    }, [])
 
     return {isLoading, error, sendRequest}
 }
